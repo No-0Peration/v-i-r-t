1edd96c14927261e6e3b1f328fc74b4f58691540
0x90 <leviathan2u@gmail.com>
2017-11-20 09:20:49 +0100

    updated

--- lib/controller.py
+++ lib/controller.py
@@ -3,17 +3,16 @@ from flask import jsonify
 import os
 
 #class which connects to a hypervisor and performs a command requested from the api
-class libvirt(Resource):
-    def run_command(self,command,hypervisor):
-        virsh_command = "virsh -c qemu+ssh://libvirtuser@{0} {1}".format(hypervisor, command)
-        print(str(virsh_command))
-        result = os.command(virsh_command)
-        return result
+def run_command(self, command,hypervisor):
+    virsh_command = "virsh -c qemu+ssh://libvirtuser@{0} {1}".format(hypervisor, command)
+    print(str(virsh_command))
+    result = os.command(virsh_command)
+    return result
 
-    def virt_install(self,command,hypervisor,customer):
-        virsh_command = "virt-install --connect=qemu+ssh://libvirtuser@{0} {1} {2}".format(hypervisor, customer, command)
-        result = os.command(virsh_command)
-        return result
+def virt_install(self, command,hypervisor,customer):
+    virsh_command = "virt-install --connect=qemu+ssh://libvirtuser@{0} {1} {2}".format(hypervisor, customer, command)
+    result = os.command(virsh_command)
+    return result
 
 # POST /controller command=<string:command>&params=<string:hypervisorID>&customer=<string:customerID>
 class api(Resource):
@@ -29,23 +28,23 @@ class api(Resource):
         if args['command'] == "list-vms":
             if args['params'] == "powered-on":
                 command = "virsh list"
-                result = libvirt.run_command(command,args['hypervisor'])
+                result = run_command(command,args['hypervisor'])
             elif args['params'] == "powered-off":
                 command = "virsh list"
-                result = libvirt.run_command(command, args['hypervisor'])
+                result = run_command(command, args['hypervisor'])
             else:
                 command = "virsh list"
-                result = libvirt.run_command(command, args['hypervisor'])
+                result = run_command(command, args['hypervisor'])
             return jsonify(result)
 
         elif args['command'] == "get-vm":
             command = "virsh list {0}".format(args['params'])
-            result = libvirt.run_command(command, args['hypervisor'], args['customer'])
+            result = run_command(command, args['hypervisor'], args['customer'])
             return jsonify(result)
 
         elif args['command'] == "vm-info":
             command = "dominfo {0}".format(args['params'])
-            result = libvirt.run_command(command, args['hypervisor'], args['customer'])
+            result = run_command(command, args['hypervisor'], args['customer'])
             return jsonify(result)
 
         elif args['command'] == "new-vm":
@@ -55,32 +54,32 @@ class api(Resource):
                 command = "virt-clone ubuntuVM"
             elif args['params'] == "debian":
                 command = "virt-clone debianVM"
-            result = libvirt.virt_install(command, args['hypervisor'], args['customer'])
+            result = virt_install(command, args['hypervisor'], args['customer'])
             return jsonify(result) # return VMnaam
 
         elif args['command'] == "start-vm":
             command = "start {0}".format(args['params'])
-            result = libvirt.run_command(command, args['hypervisor'], args['customer'])
+            result = run_command(command, args['hypervisor'], args['customer'])
             return jsonify(result)
 
         elif args['command'] == "stop-vm":
             command = "stop {0}".format(args['params'])
-            result = libvirt.run_command(command, args['hypervisor'], args['customer'])
+            result = run_command(command, args['hypervisor'], args['customer'])
             return jsonify(result)
 
         elif args['command'] == "backup-vm":
             command = "snapshot-create {0}".format(args['params'])
-            result = libvirt.run_command(command, args['hypervisor'], args['customer'])
+            result = run_command(command, args['hypervisor'], args['customer'])
             return jsonify(result)
 
         elif args['command'] == "restore-vm":
             command = "snapshot-revert {0}".format(args['params'])
-            result = libvirt.run_command(command, args['hypervisor'], args['customer'])
+            result = run_command(command, args['hypervisor'], args['customer'])
             return jsonify(result)
 
         elif args['command'] == "modify-vm":
             command = "setmem {0}".format(args['params'])
-            result = libvirt.run_command(command, args['hypervisor'], args['customer'])
+            result = run_command(command, args['hypervisor'], args['customer'])
             return jsonify(result)
 
 
