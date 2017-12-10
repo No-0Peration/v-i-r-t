739a9119e4a946af6af689ba09b6e7f03a5c6238
0x90 <leviathan2u@gmail.com>
2017-12-10 10:57:31 +0100

    finished and tested list-vms, get-vm, start-vm, stop-vm

--- libvirtapi.py
+++ libvirtapi.py
@@ -12,11 +12,13 @@ def command():
     hypervisor = request.json['hypervisor']
     vm = request.json['vm']
 
+# check if the command is for a new hypervisor, then the current connection, if so make a new connection else use current connection
     global conn, hypie
     if hypervisor != hypie:
         hypie = hypervisor
         conn = libvirt.open('qemu+ssh://{0}/system'.format(hypervisor))
 
+# command lists all vm's in a specified state: running, stopped, all
     if command == "list-vms":
         result = "{"
         domains = conn.listAllDomains(0)
@@ -49,6 +51,7 @@ def command():
                     state = 'off'
                 result += '\n\t"' + str(domain.name()) + '": "' + state + '"'
             return result + "\n}"
+    #command to start an stopped VM
     elif command == "start-vm":
         dom = conn.lookupByName(vm)
         if dom.isActive():
@@ -56,6 +59,7 @@ def command():
         else:
             dom.create()
         return {"result": "started"}
+    #command to stop an running VM
     elif command == "stop-vm":
         dom = conn.lookupByName(vm)
         if dom.isActive():
@@ -63,14 +67,17 @@ def command():
         else:
             return {"result": "Allready stopped"}
         return {"result": "stopped"}
+    #get all hardware info from an specified VM
     elif command == "get-vm":
         dom = conn.lookupByName(vm)
         state, maxmem, mem, cpus, cput = dom.info()
         return {"state": str(state), "maxMemory": str(maxmem), "currentMemory": str(mem), "numberCPU": str(cpus), "cpuTime": str(cput)}
+    #get all memory vs free memory of the hypervisor
     elif command == "get-mem":
         mem = conn.getInfo()
         memused = conn.getFreeMemory()
         return str('{"totalmem": "' + str(mem[1]) + 'MB", "memfree": "' + str(memused/1024/1024) + 'MB"}')
+    # if no commands match the request parameters, return an error
     return {"Error": "Not a valid command"}
 
 run(host='0.0.0.0', port=11111, debug=True)
