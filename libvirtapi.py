2282f6b6bce745bc5a3094d77b5bc3c6c98500a1
0x90 <leviathan2u@gmail.com>
2017-12-10 10:47:42 +0100

    finished and tested list-vms, get-vm, start-vm, stop-vm

--- libvirtapi.py
+++ libvirtapi.py
@@ -1,40 +1,8 @@
 from bottle import request, route, run
 import libvirt
-import os, re
 
-conn = libvirt.open('qemu+ssh://192.168.50.12/system')
-
-def run_command(command,hypervisor):
-    virshCommand = "virsh -c qemu+ssh://{0}/system {1}".format(hypervisor, command)
-    result = os.popen(virshCommand).read()
-    result = result.splitlines()
-    newresult = []
-    first = 0
-    for i in result[2:]:
-        if i == " ":
-            if first == 0:
-                i = ""
-                first = 1
-        else:
-            i = re.sub('[\r\n\t\f\v]', '', i)
-            first = 0
-        newresult.append(i)
-        result = newresult[0].split()
-        result = '{"id": ' + result[0] + ', "hostname": ' + '"' + result[1] + '", "state": ' + str(result[2:]).strip(
-            "[]").replace("\'", "\"") + '}'
-    return result
-
-def virt_install(command,hypervisor):
-    virsh_command = "virt-install --connect=qemu+ssh://{0}{1}".format(hypervisor, command)
-    result = response_parser(os.popen(virsh_command).read().strip("\n"))
-    return result
-
-def parse_params(params):
-    return params.split(",").replace(" ", "")
-
-def response_parser(result):
-    parsed_result = ""
-    return parsed_result
+conn = None
+hypie = ''
 
 # POST /controller command=<string:command>&vm=<string:vm name>&params=<string:additional parameters>&hypervisor=<string:hypervisorIP>
 @route('/command', method='POST')
@@ -44,45 +12,42 @@ def command():
     hypervisor = request.json['hypervisor']
     vm = request.json['vm']
 
+    global conn, hypie
+    if hypervisor != hypie:
+        hypie = hypervisor
+        conn = libvirt.open('qemu+ssh://{0}/system'.format(hypervisor))
+
     if command == "list-vms":
-        print("command recieved list-vms")
         result = "{"
-        domainIDs = conn.listDomainsID()
-        print("domids:" + str(domainIDs))
-        if domainIDs == None:
-            print('Failed to get a list of domain IDs')
+        domains = conn.listAllDomains(0)
+
+        if domains == None:
             return {"error": "There are no VM's on this hypervisor"}
         if "running" in params:
-            for domainID in domainIDs:
-                print(domainID)
-                dom = conn.lookupByID(domainID)
-                print(dom.isActive())
-                if dom.isActive():
+            for domain in domains:
+                if domain.isActive():
                     state = 'running'
                 else:
                     state = 'off'
                 if state == 'running':
-                    print('in running')
-                    result += '\n\t"'+ str(dom.name()) + '": "' + state + '"'
+                    result += '\n\t"' + str(domain.name()) + '": "' + state + '"'
             return result + "\n}"
         elif "stopped" in params:
-            for domainID in domainIDs:
-                dom = conn.lookupByID(domainID)
-                if dom.isActive():
+            for domain in domains:
+                if domain.isActive():
                     state = 'running'
                 else:
                     state = 'off'
                 if state == 'off':
-                    result += '\n\t"'+ str(dom.name()) + '": "' + state + '"'
+                    result += '\n\t"' + str(domain.name()) + '": "' + state + '"'
             return result + "\n}"
         elif "all" in params:
-            for domainID in domainIDs:
-                dom = conn.lookupByID(domainID)
-                if dom.isActive():
+            for domain in domains:
+                if domain.isActive():
                     state = 'running'
                 else:
                     state = 'off'
-                result += '\n\t"'+ str(dom.name()) + '": "' + state + '"'
+                result += '\n\t"' + str(domain.name()) + '": "' + state + '"'
             return result + "\n}"
     elif command == "start-vm":
         dom = conn.lookupByName(vm)
@@ -96,12 +61,16 @@ def command():
         if dom.isActive():
             dom.destroy()
         else:
-            return {"result": "Allready started"}
+            return {"result": "Allready stopped"}
         return {"result": "stopped"}
+    elif command == "get-vm":
+        dom = conn.lookupByName(vm)
+        state, maxmem, mem, cpus, cput = dom.info()
+        return {"state": str(state), "maxMemory": str(maxmem), "currentMemory": str(mem), "numberCPU": str(cpus), "cpuTime": str(cput)}
     elif command == "get-mem":
         mem = conn.getInfo()
         memused = conn.getFreeMemory()
         return str('{"totalmem": "' + str(mem[1]) + 'MB", "memfree": "' + str(memused/1024/1024) + 'MB"}')
-    return run_command(virshCommand, hypervisor)
+    return {"Error": "Not a valid command"}
 
 run(host='0.0.0.0', port=11111, debug=True)
